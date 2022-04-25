ef98e2e0547e31372eed5d85228659cb72c43417
IwanTz <81747549+iwansal64@users.noreply.github.com>
2022-04-25 15:09:21 +0700

    Update biar lebih bagus ya ges ya
    gila dulu kodingan aku jelek bat wkwkwk

--- kodingan.py
+++ kodingan.py
@@ -1,11 +1,18 @@
-data_user = input('y/n')
-print("Ayam?")
-if(data_user == 'y'):
-  print('anjay')
-  print('jago bgt si')
-  
- if(data_user == 'n'):
-  print('oh yaudah oke')
-  print('emm oke')
+import random
+while True:
+  makanan = random.choice(["Ayam?", "Nasgor?", "Lele?"])
+  print(makanan)
+  data_user = input('y/n')
+  if(data_user == 'y'):
+    print('okehh.. nih ', makanan)
+    print('mau makan lagi gak?')
+    data = input('y/n')
+    if data == 'y':
+      continue
+    else:
+      break
+    
+  elif(data_user == 'n'):
+    print('oh yaudah oke')
+    break
 
-  print('makan?')
